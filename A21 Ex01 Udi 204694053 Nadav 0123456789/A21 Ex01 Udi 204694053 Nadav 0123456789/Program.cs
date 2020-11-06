462232d3df204d828003c3f459667a7c7321e294
UdiRahav <rahavudi93@gmail.com>
2020-11-06 15:01:01 +0200

    first commit

---
+++ A21 Ex01 Udi 204694053 Nadav 0123456789/A21 Ex01 Udi 204694053 Nadav 0123456789/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A21_Ex01_Udi_204694053_Nadav_0123456789
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
